d504330bdf13846eb8983f51f9d63c2473dabb0e
zahidali-dev <your-github-email@example.com>
2025-11-19 14:20:02 +0500

    Resolve merge conflict

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-  base: '/', // 👈 Make sure this is here
-})
-=======
-  base: '/',   // 👈 IMPORTANT FOR VERCEL
+  base: '/',
 });
-
->>>>>>> 182c8db655070eed8fac32e6afa1e0ed79a760f7
